5bf5d2ca005542106283d2a14240aacab08e750d
Dave <denright@outlook.com>
2024-02-24 10:09:45 +0800

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+// Load script when Foundry is ready.
+Hooks.on("ready", function() {
+    console.log("Ready to load Dave's addons");
+  });
+
+// Create damage automation workflow.
+
+// If attack roll successful, roll damage
+
+// Ask player to do determination test.
+
+// Remove shock or add wounds depending on determination result.
+// Token appears to be under wng.actor.WrathAndGloryActor
